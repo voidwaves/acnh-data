ffca8ed27171f92064cac613db7092071c749853
voidwaves <incognitoxyx@googlemail.com>
2020-05-10 23:13:09 +0200

    refactored the hard coded data and types into their own file

--- Item.ts
+++ Item.ts
@@ -1,88 +1,10 @@
-
 // to run use the command:
 // ts-node -O "{ \"target\": \"es2017\" }" Item.ts
 
-type Category = 
-    'Material' |
-    'Nook Item' |
-    'Tool' |
-    'Furniture'
-
-interface Ingredient {
-    itemid: number
-    amount: number
-}
-
-interface Item {
-    id: number
-    name: string
-    category: Category
-    recipe: Ingredient[]
-}
-
-const DataBase: Item[] = [
-    {
-        id: 1,
-        name: 'Tree Branch',
-        category: 'Material',
-        recipe: []
-    },
-    {
-        id: 2,
-        name: 'Stone',
-        category: 'Material',
-        recipe: []
-    },
-    {
-        id: 3,
-        name: 'Wood',
-        category: 'Material',
-        recipe: []
-    },
-    {
-        id: 4,
-        name: 'Iron Nugget',
-        category: 'Material',
-        recipe: []
-    },
-    {
-        id: 5,
-        name: 'Gold Nugget',
-        category: 'Material',
-        recipe: []
-    },
-    {
-        id: 6,
-        name: 'Flimsy Axe',
-        category: 'Tool',
-        recipe: [
-            {itemid: 1, amount: 5},
-            {itemid: 2, amount: 1}
-        ]
-    },
-    {
-        id: 7,
-        name: 'Axe',
-        category: 'Tool',
-        recipe: [
-            {itemid: 6, amount: 1},
-            {itemid: 3, amount: 3},
-            {itemid: 4, amount: 1}
-        ]
-    },
-    {
-        id: 8,
-        name: 'Golden Axe',
-        category: 'Tool',
-        recipe: [
-            {itemid: 7, amount: 1},
-            {itemid: 5, amount: 1}
-        ]
-    }
-]
+import { DataBase, Ingredient, Item } from "./Database"
 
 function getItemById(id: number): Item {
-    return DataBase.find(item => item.id === id)
+    return DataBase.items.find(item => item.id === id)
 }
 
 function flatten<Type>(array: Type[][]): Type[] {
